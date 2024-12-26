6a944547d4aaa47df9ebbf058470d9dacf582ff8
Jeff Lloyd <jlloyd0539@gmail.com>
2024-12-25 20:34:21 -0600

    chore(ui): add categories and grouped list by first letter of contact name

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.scss'
-import { ContactWithId } from 'schema';
+import { ListContact } from 'schema';
 import { z } from 'zod';
 import Panel from './components/Panel/Panel';
 import BackButton from './components/BackButton/BackButton';
 import ContactCategory from './components/ContactCategory/ContactCategory';
 import Contact from './components/Contact/Contact';
 
-type ContactType = z.infer<typeof ContactWithId>;
+type ContactType = z.infer<typeof ListContact>;
 
 function App() {
   const [contacts, setContacts] = useState<ContactType[]>([]);
+  const [filteredContacts, setFilteredContacts] = useState<ContactType[]>([]);
   const [showSidebar, setShowSidebar] = useState(false);
+  const [activeCategory, setActiveCategory] = useState<string | null>("all");
+  const [contactIsActive, setContactIsActive] = useState<number | null>();
   const hasFetched = useRef(false);
+  
   useEffect(() => {
     if (hasFetched.current) {
       return;
@@ -21,14 +25,55 @@ function App() {
     getContacts();
   },[]);
 
+  useEffect(() => {
+    setFilteredContacts(contacts);
+  }, [contacts]);
+
   const getContacts = async () => {
-    const response = await fetch('http://localhost:3000/contacts/');
+    const response = await fetch('http://localhost:3000/contacts/list');
     const data = await response.json();
-    const result = ContactWithId.array().safeParse(data.data);
+    const result = ListContact.array().safeParse(data.data);
     if (!result.success) {
       throw new Error(`Invalid person data: ${result.error}`);
     }
     setContacts(result.data);
+  }
+
+  const groupedContacts = (contacts: ContactType[]) => {
+    const grouped = contacts.reduce((acc: { [key: string]: ContactType[] }, contact) => {
+      const firstLetter = contact.fname[0].toUpperCase();
+      if (!acc[firstLetter]) {
+        acc[firstLetter] = [];
+      }
+      acc[firstLetter].push(contact);
+      return acc;
+    }, {});
+    return grouped;
+  }
+
+  const getContactRoles = (contacts: ContactType[]) => {
+    const grouped = contacts.reduce((acc: { [key: string]: { contacts: ContactType[], count: number } }, contact) => {
+      const role = contact.role || 'Unknown';
+      if (!acc[role]) {
+        acc[role] = { contacts: [], count: 0 };
+      }
+      acc[role].contacts.push(contact);
+      acc[role].count++;
+      return acc;
+    }, {});
+    return grouped;
+  }
+
+  const handleCategoryClick = (category: string) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setFilteredContacts(contacts);
+    } else if (category === "favorites") {
+      setFilteredContacts(contacts.filter((contact) => contact.is_favorite));
+    } else {
+      setFilteredContacts(getContactRoles(contacts)[category].contacts);
+    }
+    setShowSidebar(!showSidebar);
   }
 
   return (
@@ -36,14 +81,33 @@ function App() {
       <nav className="sidebar" data-show={showSidebar}>
         <Panel>
           <ul className="contact-category-list">
-            <ContactCategory
-              onClick={() => setShowSidebar(!showSidebar)}
-              badgeText="2"
-              active={true}
-              category="all"
-            >
-              All
-            </ContactCategory>
+              <ContactCategory
+                onClick={() => handleCategoryClick("all")}
+                badgeText={contacts.length.toString()}
+                active={activeCategory === "all"}
+                category="all"
+              >
+                All Contacts
+              </ContactCategory>
+              <ContactCategory
+                onClick={() => handleCategoryClick("favorites")}
+                badgeText={contacts.filter((contact) => contact.is_favorite).length.toString()}
+                active={activeCategory === "favorites"}
+                category="favorites"
+              >
+                My Favorites
+              </ContactCategory>
+            { Object.keys(getContactRoles(contacts)).map((role) => (
+              <ContactCategory
+                onClick={() => handleCategoryClick(role)}
+                badgeText={getContactRoles(contacts)[role].count.toString()}
+                active={activeCategory === role}
+                category={role}
+                key={role}
+              >
+                {role}
+              </ContactCategory>
+            )) }
           </ul>
         </Panel>
       </nav>
@@ -51,11 +115,27 @@ function App() {
       <div className="content" data-show={!showSidebar}>
         <Panel overflowHidden={true}>
           <div className="content__header">
-            <BackButton onClick={() => setShowSidebar(!showSidebar)}>Categories</BackButton>
+            <BackButton 
+              onClick={() => setShowSidebar(!showSidebar)}
+            >
+              Categories
+            </BackButton>
           </div>
           <ul className="contact-list">
-            { contacts?.map(contact => (
-              <Contact active={contact.id === 1} contact={contact} key={contact.id} />
+            { Object.keys(groupedContacts(filteredContacts)).map((letter) => (
+              <li key={letter}>
+                <h2>{letter}</h2>
+                <ul>
+                  { groupedContacts(filteredContacts)[letter].map((contact) => (
+                    <Contact 
+                      active={contactIsActive === contact.id}
+                      contact={contact}
+                      key={contact.id}
+                      onClick={() => setContactIsActive(contact.id)}
+                    />
+                  )) }
+                </ul>
+              </li>
             )) }
           </ul>
         </Panel>
