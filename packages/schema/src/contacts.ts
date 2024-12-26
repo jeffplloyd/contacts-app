2629988b92479e6fa82946a85dcd63707f30c023
Jeff Lloyd <jlloyd0539@gmail.com>
2024-12-25 20:32:17 -0600

    chore(api): address validation issues

--- packages/schema/src/contacts.ts
+++ packages/schema/src/contacts.ts
@@ -1,20 +1,21 @@
 import {z} from "zod";
 
-export const Contact = z.object({
+export const ListContact = z.object({
+  id: z.number().optional(),
   fname: z.string().max(50),
   lname: z.string().max(50),
-  dob: z.string().nullable().optional(),
+  is_favorite: z.boolean().optional(),
+  role: z.string().max(50).optional(),
+});
+
+export const Contact = ListContact.extend({
+  dob: z.date().nullable().optional(),
   website: z.string().max(100).url().nullable().optional(),
   personal_email: z.string().max(100).email().nullable().optional(),
   personal_phone: z.string().max(20).nullable().optional(),
   work_email: z.string().max(100).email().nullable().optional(),
   work_phone: z.string().max(20).nullable().optional(),
-  is_favorite: z.boolean().optional(),
-  role_id: z.number().nullable().optional(),
-  created_at: z.string().optional(),
-  updated_at: z.string().optional(),
-});
-
-export const ContactWithId = Contact.extend({
-  id: z.number(),
+  role_id: z.number().optional(),
+  created_at: z.date().optional(),
+  updated_at: z.date().optional(),
 });
