2e7f103c390ef842765d4cec576bdd1ccdc052c0
Jeff Lloyd <jlloyd0539@gmail.com>
2024-12-30 21:50:39 -0600

    chore(api): address CORS error with methods

--- api/src/index.ts
+++ api/src/index.ts
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 app.use("/contacts", contactsRoutes);
